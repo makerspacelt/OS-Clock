aae6f8fb6417935f43045530b85e261bba1143f4
Darius Kasiulevičius <1218606+dariuskasiulevicius@users.noreply.github.com>
2018-08-15 13:54:19 +0300

    Fixed minus time calculation

--- firmware/main.c
+++ firmware/main.c
@@ -538,8 +538,8 @@ void calculateTime(void)
         deviceSetting.status = STATUS_ZERO_TIME_START;
         saveSettings();
     }
-    if (deviceSetting.status == STATUS_MINUS_TIME_START && realTime < deviceSetting.zeroTime) {
-        realTime = deviceSetting.zeroTime - realTime;
+    if (deviceSetting.status == STATUS_MINUS_TIME_START && realTime > deviceSetting.zeroTime) {
+        realTime = 86399 - realTime + deviceSetting.zeroTime;
         isMinus = TRUE;
     } else if (realTime < deviceSetting.zeroTime) {
         realTime = 86400 - deviceSetting.zeroTime + realTime;
